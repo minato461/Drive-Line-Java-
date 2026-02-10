de4b17075c99c54ac29b6ac6eb98690f46ff5c65
スーハイ <sanku461@gmail.com>
2026-02-10 17:02:52 +0900

    理解を深めるためにemp2追記

--- Practice/src/curriculum/e/Employee.java
+++ Practice/src/curriculum/e/Employee.java
@@ -16,7 +16,12 @@ public class Employee {
 		emp.name = "山田太郎";
 		// メゾッドの呼び出し
 		emp.work();
+		
+		// 理解を深めるために2人目を追加
+		Employee emp2 = new Employee();
+		emp2.name = "佐藤花子";
+		emp2.work();
 	}
-
+	
 
 }
